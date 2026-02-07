ae937115fa9d763f8340d0f0460c844998fe3bd3
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-07 09:48:45 +0530

    Add MatrixUtils to test

---
+++ math-test/src/utils/MatrixUtils.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <matrix/Matrix3D.h>
+
+namespace TestUtils
+{
+	
+	static unsigned int SIZE = 9;
+	static unsigned int ROW_SIZE = 3;
+
+	template<typename T>
+	void EXPECT_MAT3D_EQ(math::Matrix3D<T> expected, math::Matrix3D<T> actual)
+	{
+		for (unsigned int i = 0; i < SIZE; i++)
+		{
+			EXPECT_FLOAT_EQ(expected(i / ROW_SIZE, i % ROW_SIZE), actual(i / ROW_SIZE, i % ROW_SIZE));
+		}
+	}
+
+	template<typename T>
+	void EXPECT_MAT3D_IDENTITY(math::Matrix3D<T> actual)
+	{
+		const math::Matrix3D<T> identity(
+			T(1), T(0), T(0),
+			T(0), T(1), T(0),
+			T(0), T(0), T(1)
+		);
+
+		for (unsigned int i = 0; i < SIZE; i++)
+		{
+			EXPECT_FLOAT_EQ(identity(i / ROW_SIZE, i % ROW_SIZE), actual(i / ROW_SIZE, i % ROW_SIZE));
+		}
+	}
+}
