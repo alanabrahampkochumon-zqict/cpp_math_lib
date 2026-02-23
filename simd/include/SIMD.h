da2bb48d25e64489381ce70fa6744be9479f2de9
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-23 10:28:25 +0530

    Add SIMD Macros

---
+++ simd/include/SIMD.h
@@ -0,0 +1,36 @@
+#pragma once
+
+/*************************************
+ *                                   *
+ *            PREPROCESSORS          *
+ *                                   *
+ *************************************/
+ /**
+  * Defining FORCE_SSE will turn on SSE_4_1 even if the hardware supports newer instruction set
+  * Similar MACROS are FORCE_AVX, FORCE_AVX2, FORCE_AVX512, and FORCE_SCALAR which will turn off SIMD.
+  * Although you will be able to see the defined macros, all functions and register types will not be available or substituted with stubs.
+  */
+#ifdef __AVX512__
+	#define FALCON_AVX512_SUPPORTED
+#endif
+
+#ifdef __AVX2__
+	#define FALCON_AVX2_SUPPORTED
+#endif
+
+#ifdef __AVX__
+	#define FALCON_AVX_SUPPORTED
+#endif
+
+#if defined(__SSE__) || defined(__SSE4_1__) || defined(_M_X64)
+	#define FALCON_SSE_SUPPORTED
+	#define FALCON_SIMD_SUPPORTED // If atleast SSE is supported, then SIMD will be available.
+#endif
+
+namespace falcon::simd
+{
+	// TODO: Add Neon Support Macros
+
+	
+
+}
