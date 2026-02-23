52b1a402959fb41aef78f198f68f264246a8d769
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-23 10:45:38 +0530

    Add SIMDReg

--- simd/include/SIMD.h
+++ simd/include/SIMD.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <cstddef>
+#include <immintrin.h>
+#include <zmmintrin.h>
+
 /*************************************
  *                                   *
  *            PREPROCESSORS          *
@@ -10,7 +14,7 @@
   * Similar MACROS are FORCE_AVX, FORCE_AVX2, FORCE_AVX512, and FORCE_SCALAR which will turn off SIMD.
   * Although you will be able to see the defined macros, all functions and register types will not be available or substituted with stubs.
   */
-#ifdef __AVX512__
+#ifdef __AVX512F__
 	#define FALCON_AVX512_SUPPORTED
 #endif
 
@@ -29,8 +33,19 @@
 
 namespace falcon::simd
 {
-	// TODO: Add Neon Support Macros
+	template <typename T>
+	struct SIMDReg {};
 
-	
+	template<> struct SIMDReg<int>
+	{	
+		#if defined(FALCON_AVX512_SUPPORTED) && (!defined(FORCE_AVX2) || !defined(FORCE_AVX) || !defined(FORCE_SSE) || !defined(FORCE_SCALAR))
+			using reg = __m512i;
+		#elif (defined(FALCOM_AVX2_SUPPORTED) || defined(FALCON_AVX_SUPPORTED)) && (!defined(FORCE_SSE) || !defined(FORCE_SCALAR))
+			using reg = __m256i;
+		elif defined(FALCON_SSE_SUPPORTED) && !defined(FORCE_SCALAR)
+			using reg = __m128i;
+		#endif
+	};
+
 
 }
