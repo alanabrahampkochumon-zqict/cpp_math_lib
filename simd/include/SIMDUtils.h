79183236f81fd899129a758cce5e8c5fad9d246f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-04 10:52:44 +0530

    Add registerwidth and type tests to SIMDUtil

--- simd/include/SIMDUtils.h
+++ simd/include/SIMDUtils.h
@@ -9,15 +9,24 @@ namespace falcon::simd
 	{
 		std::size_t alignedByteSize;
 		std::size_t padding;
+		std::size_t packedRegisterWidth;
+		std::size_t registerCount;
 	};
 
-	constexpr PackedSize calculatePackedSize(std::size_t totalByteSize)
+	/**
+	 * A simple calculator function that gives Aligned Byte Size, Padding, Packed Register Width, and Register Count
+	 * when provided with an unaligned byteSize and maximumAlignment possible.
+	 * @param totalByteSize Total unaligned byte size.
+	 * @param maxAlignAs Max possible alignment for a given CPU architecture. 16 for SSE, 32 for AVX and AVX2, and 64 for AVX512
+	 * @return PackedSize containing alignedByteSize, padding, register width and register count.
+	 */
+	constexpr PackedSize calculatePackedSize(std::size_t totalByteSize, std::size_t maxAlignAs)
 	{
 		if (totalByteSize < 16)
-			return PackedSize { 16, 16 - totalByteSize };
+			return PackedSize { 16, 16 - totalByteSize, 0, 0 };
 		
 		const std::size_t packedSize = std::bit_ceil(totalByteSize);
 
-		return PackedSize { packedSize, packedSize - totalByteSize };
+		return PackedSize { packedSize, packedSize - totalByteSize, 0, 0 };
 	}
 }
--- test-suite/src/simd/SimdUtilsTests.cpp
+++ test-suite/src/simd/SimdUtilsTests.cpp
@@ -4,8 +4,11 @@
 struct TestData
 {
 	std::size_t totalByteSize;
+	std::size_t alignAs;
 	std::size_t expectedByteSize;
 	std::size_t padding;
+	std::size_t packedRegisterWidth;
+	std::size_t registerCount;
 };
 
 class PackedSizeCalculatorParams: public ::testing::TestWithParam<TestData>{};
@@ -13,66 +16,81 @@ class PackedSizeCalculatorParams: public ::testing::TestWithParam<TestData>{};
 TEST_P(PackedSizeCalculatorParams, CalculatesCorrectSize)
 {
 	// Given an arbitrary data type size and total byte size
-	const auto [totalByteSize, expectedByteSize, expectedPadding] = GetParam();
+	const auto [totalByteSize, alignAs, expectedByteSize, expectedPadding, packedRegisterWidth, registerCount] = GetParam();
 
 	// When packed size is calculated
-	falcon::simd::PackedSize packedSize = falcon::simd::calculatePackedSize(totalByteSize);
+	falcon::simd::PackedSize packedSize = falcon::simd::calculatePackedSize(totalByteSize, alignAs);
 
 	// It gives the nearest packed size and padding
 	EXPECT_EQ(expectedByteSize, packedSize.alignedByteSize);
 	EXPECT_EQ(expectedPadding, packedSize.padding);
+	// TODO: Add expected values to register width and num
 }
 
 INSTANTIATE_TEST_SUITE_P(
 	PackedSizeCalculatorTests,
 	PackedSizeCalculatorParams,
 	::testing::Values(
-		TestData {1, 16, 15 }, 
-		TestData {4, 16, 12 }, 
-		TestData {  8, 16, 8 }, 
-		TestData { 12, 16, 4 }, 
-		TestData { 16, 16, 0 }, 
-		TestData {  20, 32, 12 }, 
-		TestData {  24, 32, 8 },
-		TestData {  32, 32, 0 },
-		TestData {  48, 64, 16 },
-		TestData {  56, 64, 8 },
-		TestData {  64, 64, 0 },
-		TestData {  72, 128, 56 },
-		TestData {  80, 128, 48 },
-		TestData {  128, 128, 0 },
-		TestData {  192, 256, 64 },
-		TestData {  256, 256, 0 },
-		TestData {  512, 512, 0 },
-		TestData {  777, 1024, 247 },
-		TestData {  1024, 1024, 0 }
-	)
+		// SSE
+		TestData {1, 16, 16, 15, 16, 1 }, 
+		TestData {4, 16,16, 12, 16, 1 },
+		TestData {  8, 16,16, 8, 16, 1 },
+		TestData { 12,16, 16, 4 , 16, 1},
+		TestData { 16, 16,16, 0 , 16, 1},
+		TestData {  20,16, 32, 12 , 16 , 2},
+		TestData {  24,16, 32, 8, 16, 2 },
+		TestData {  32,16, 32, 0 , 16, 2},
+		TestData {  48,16, 64, 16 , 16, 4},
+		TestData {  56,16, 64, 8, 16, 4 },
+		TestData {  64,16, 64, 0 , 16, 4},
+		TestData {  72, 16,128, 56 ,16, 8},
+		TestData {  80, 16,128, 48 , 16, 8},
+		TestData {  128, 16,128, 0 ,16, 8},
+		TestData {  192,16, 256, 64 , 16, 16},
+		TestData {  256,16, 256, 0 , 16, 16},
+		TestData {  512, 16,512, 0 , 16, 32},
+		TestData {  777,16, 1024, 247 , 16, 64},
+		TestData {  1024,16, 1024, 0, 16, 64 },
+		// AVX/AVX2
+		TestData { 1, 32, 16, 15, 16, 1 },
+		TestData { 4, 32,16, 12, 16, 1 },
+		TestData { 8, 32,16, 8, 16, 1 },
+		TestData { 12,32, 16, 4 , 16, 1 },
+		TestData { 16, 32,16, 0 , 16, 1 },
+		TestData { 20,32, 32, 12 , 32 , 1 },
+		TestData { 24,32, 32, 8, 32, 1 },
+		TestData { 32,32, 32, 0 , 32, 1 },
+		TestData { 48,32, 64, 16 , 32, 2 },
+		TestData { 56,32, 64, 8, 32, 2 },
+		TestData { 64,32, 64, 0 , 32, 2 },
+		TestData { 72, 32,128, 56 ,32, 4 },
+		TestData { 80, 32,128, 48 , 32, 4 },
+		TestData { 128, 32,128, 0 ,32, 4 },
+		TestData { 192,32, 256, 64 , 32,  8},
+		TestData { 256,32, 256, 0 , 32, 8 },
+		TestData { 512, 32,512, 0 , 32, 16 },
+		TestData { 777,32, 1024, 247 , 32, 32 },
+		TestData { 1024,32, 1024, 0, 32, 32 },
+		// AVX512
+		TestData { 1, 64, 16, 15, 16, 1 },
+		TestData { 4, 64,16, 12, 16, 1 },
+		TestData { 8, 64,16, 8, 16, 1 },
+		TestData { 12,64, 16, 4 , 16, 1 },
+		TestData { 16, 64,16, 0 , 16, 1 },
+		TestData { 20,64, 32, 12 , 32 , 1 },
+		TestData { 24,64, 32, 8, 32, 1 },
+		TestData { 32,64, 32, 0 , 32, 1 },
+		TestData { 48,64, 64, 16 , 64, 1 },
+		TestData { 56,64, 64, 8, 64, 1 },
+		TestData { 64,64, 64, 0 , 64, 1 },
+		TestData { 72, 64,128, 56 ,64, 2 },
+		TestData { 80, 64,128, 48 , 64, 2 },
+		TestData { 128, 64,128, 0 ,64, 2},
+		TestData { 192,64, 256, 64 , 64,  4 },
+		TestData { 256,64, 256, 0 , 64, 4 },
+		TestData { 512, 64,512, 0 , 64, 8 },
+		TestData { 777,64, 1024, 247 , 64, 16 },
+		TestData { 1024,64, 1024, 0, 64, 16 }
 
-//INSTANTIATE_TEST_SUITE_P(
-//	PackedSizeCalculatorTests,
-//	PackedSizeCalculatorParams,
-//	::testing::Values(
-//		TestData{sizeof(float), 4, 16, 12}, // 1d float vec
-//		TestData{sizeof(float),  8, 16, 8}, // 2d float vec
-//		TestData{sizeof(float), 12, 16, 4}, // 3d float vec
-//		TestData{sizeof(float), 16, 16, 0}, // 4d float vec
-//		TestData { sizeof(double), 8, 16, 8}, // 1d double vec
-//		TestData { sizeof(double), 16, 16, 0}, // 1d double vec
-//		TestData { sizeof(double), 24, 32, 8}, // 3d double vec
-//		TestData { sizeof(double), 32, 32, 0}, // 4d double vec
-//
-//		TestData{sizeof(char), 9, 16}, // 3x3 char matrix
-//		TestData{sizeof(short), 18, 32}, // 3x3 short matrix
-//		TestData{sizeof(float), 36, 64}, // 3x3 float matrix
-//		TestData{sizeof(double), 72, 128}, // 3x3 double matrix
-//
-//		TestData{sizeof(double), 128, 128}, // 4x4 double matrix
-//		TestData{sizeof(double), 96, 128}, // 3x4 double matrix
-//		TestData{sizeof(unsigned char), 16, 16}, // 4x4 uchar matrix
-//		TestData{sizeof(float), 64, 64}, // 4x4 float matrix
-//		TestData{sizeof(short), 32, 32}, // 4x4 short matrix
-//		TestData{sizeof(char), 17, 32}, // 1 byte over 16 byte limit
-//		TestData{sizeof(char), 33, 64}, // 1 byte over 32 byte limit
-//		TestData{sizeof(char), 65, 128} // 1 byte over 64 byte limit
-//	)
+	)
 );
