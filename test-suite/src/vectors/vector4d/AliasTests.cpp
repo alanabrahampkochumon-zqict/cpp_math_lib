578e974756495551b71417337360805d6666e3d9
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-06 09:58:32 +0530

    Convert Vec4DHelpers to alias static test

---
+++ test-suite/src/vectors/vector4d/AliasTests.cpp
@@ -0,0 +1,43 @@
+#include "Vector4DTestSetup.h"
+
+TEST(Vector4DHelper, bVec4IsA4DBoolVector)
+{
+	static_assert(std::is_same_v<math::bVec4::value_type, bool> && "bVec4 must contain boolean elements");
+	static_assert(std::is_same_v<math::bVec4, math::Vector4D<bool>> && "bVec4 must be an alias of Vector4D<bool>");
+}
+
+TEST(Vector4DHelper, iVec4IsA4DIntVector)
+{
+	static_assert(std::is_same_v<math::iVec4::value_type, int> && "iVec4 must contain int elements");
+	static_assert(std::is_same_v<math::iVec4, math::Vector4D<int>> && "iVec4 must be an alias of Vector4D<int>");
+}
+
+TEST(Vector4DHelper, uVec4IsA4DUnsignedIntVector)
+{
+	static_assert(std::is_same_v<math::uVec4::value_type, unsigned int> && "uVec4 must contain unsigned int elements");
+	static_assert(std::is_same_v<math::uVec4, math::Vector4D<unsigned int>> && "uVec4 must be an alias of Vector4D<unsigned int>");
+}
+
+TEST(Vector4DHelper, vec4IsA4DFloatVector)
+{
+	static_assert(std::is_same_v<math::vec4::value_type, float> && "vec4 must contain float elements");
+	static_assert(std::is_same_v<math::vec4, math::Vector4D<float>> && "vec4 must be an alias of Vector4D<float>");
+}
+
+TEST(Vector4DHelper, lVec4IsA4DLongLongVector)
+{
+	static_assert(std::is_same_v<math::lVec4::value_type, long long> && "lVec4 must contain long long elements");
+	static_assert(std::is_same_v<math::lVec4, math::Vector4D<long long>> && "lVec4 must be an alias of Vector4D<long long>");
+}
+
+TEST(Vector4DHelper, dVec4IsA4DDoubleVector)
+{
+	static_assert(std::is_same_v<math::dVec4::value_type, double> && "dVec4 must contain double elements");
+	static_assert(std::is_same_v<math::dVec4, math::Vector4D<double>> && "dVec4 must be an alias of Vector4D<double>");
+}
+
+TEST(Vector4DHelper, ulVec4IsA4DUnsignedLongLongVector)
+{
+	static_assert(std::is_same_v<math::ulVec4::value_type, unsigned long long> && "ulVec4 must contain unsigned long long elements");
+	static_assert(std::is_same_v<math::ulVec4, math::Vector4D<unsigned long long>> && "ulVec4 must be an alias of Vector4D<unsigned long long>");
+}
