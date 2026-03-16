e16d1ce03f18b541f9693908ef5aeea79edd6e92
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-16 09:15:22 +0530

    feat: add configuration for print precision

---
+++ math-lib/include/Config.h
@@ -0,0 +1,13 @@
+#pragma once
+/**
+ * @file Config.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: March 16, 2026
+ *
+ * @brief Configuration for library wide options.
+ * 
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+static inline constexpr unsigned int PRINT_PRECISION = 5; ///< Floating point precision for std::floating point numbers. Used only for displaying.
+
