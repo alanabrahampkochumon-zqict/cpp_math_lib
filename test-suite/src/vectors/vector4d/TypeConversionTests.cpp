a4131a3a220254b72d341229d722e78dc31ccb9a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-16 10:29:39 +0530

    test(Vector4D): Separate conversion constructor tests to test only one concern

--- test-suite/src/vectors/vector4d/TypeConversionTests.cpp
+++ test-suite/src/vectors/vector4d/TypeConversionTests.cpp
@@ -3,7 +3,7 @@
  * @author Alan Abraham P Kochumon
  * @date Created on: March 07, 2026
  *
- * @brief Verifies @ref Vector4D<bool> conversion constructor logic.
+ * @brief Verifies @ref Vector4D conversion constructor logic.
  *
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
@@ -11,51 +11,45 @@
 
 #include "Vector4DTestSetup.h"
 
+using testutils::EXPECT_VEQ_CONTAINS;
 
+
+/** @test Verify conversion constructor promotes type from float to double */
 TEST(Vector4DConversionConstructor, ConversionConstructorCreatesNewVectorWithPromotedType)
 {
+    const fgm::Vector4D vec1(3.0f, 1.0f, 6.0f, 2.0f);
+
+    const fgm::Vector4D<double> vec2(vec1);
+
+    static_assert(std::is_same_v<decltype(vec2)::value_type, double>);
+}
+
+
+/** @test Verify conversion constructor returns new instance */
+TEST(Vector4DConversionConstructor, ReturnsNewInstance)
+{   
     // Given a float vector
     fgm::Vector4D vec1(3.0f, 1.0f, 6.0f, 2.0f);
 
-    // When it is converted to a double vector
+    // When converted to a double vector
     fgm::Vector4D<double> vec2(vec1);
-    // And some values of new vector is mutated
+    // And one of its value mutated
     vec2.x = 5;
 
     // Then, the float vector remains unchanged
-    ASSERT_FLOAT_EQ(3.0f, vec1.x);
-    ASSERT_FLOAT_EQ(1.0f, vec1.y);
-    ASSERT_FLOAT_EQ(6.0f, vec1.z);
-    ASSERT_FLOAT_EQ(2.0f, vec1.w);
+    EXPECT_VEQ_CONTAINS(vec1, 3.0f, 1.0f, 6.0f, 2.0f);
 
     // And, the new vector is of type double with the mutated value
-    static_assert(std::is_same_v<decltype(vec2)::value_type, double>);
-    ASSERT_DOUBLE_EQ(5.0, vec2.x);
-    ASSERT_DOUBLE_EQ(1.0, vec2.y);
-    ASSERT_DOUBLE_EQ(6.0, vec2.z);
-    ASSERT_DOUBLE_EQ(2.0, vec2.w);
+    EXPECT_VEQ_CONTAINS(vec2, 5.0, 1.0, 6.0, 2.0);
 }
 
+
+/** @test Verify conversion constructor demotes type from double to float */
 TEST(Vector4DConversionConstructor, ConversionConstructorCreatesNewVectorWithDemotedType)
 {
-    // Given a double vector
-    fgm::Vector4D vec1(3.0, 1.0, 6.0, 2.0);
+    const fgm::Vector4D vec1(3.0, 1.0, 6.0, 2.0);
 
-    // When it is converted to a float vector
-    fgm::Vector4D<float> vec2(vec1);
-    // And some values of new vector is mutated
-    vec2.x = 5;
+    const fgm::Vector4D<float> vec2(vec1);
 
-    // Then, the float vector remains unchanged
-    ASSERT_DOUBLE_EQ(3.0, vec1.x);
-    ASSERT_DOUBLE_EQ(1.0, vec1.y);
-    ASSERT_DOUBLE_EQ(6.0, vec1.z);
-    ASSERT_DOUBLE_EQ(2.0, vec1.w);
-
-    // And, the new vector is of type float with the mutated value
     static_assert(std::is_same_v<decltype(vec2)::value_type, float>);
-    ASSERT_FLOAT_EQ(5.0f, vec2.x);
-    ASSERT_FLOAT_EQ(1.0f, vec2.y);
-    ASSERT_FLOAT_EQ(6.0f, vec2.z);
-    ASSERT_FLOAT_EQ(2.0f, vec2.w);
 }
